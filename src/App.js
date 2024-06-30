32e86ea194742718a8b411e8ea1302cabed8b2da
unknown <Louis Pierre Andre>
2024-06-30 08:29:03 -0400

    Description de la modification

--- src/App.js
+++ src/App.js
@@ -23,3 +23,26 @@ function App() {
 }
 
 export default App;
+
+
+// git init
+// git add .
+// git commit - m "Initial commit"
+// git remote add origin https://github.com/LouisPierre-Andre/website30.git
+// git branch - M main
+// git push - u origin main
+
+
+
+
+// git add.  # Ajoute tous les fichiers modifiés
+// git commit - m "Description de la modification"  # Fait un commit avec un message descriptif
+// git push origin main  # Pousse les modifications vers GitHub sur la branche principale(main)
+
+
+
+
+
+// npm install netlify - cms - app--force
+// npm install netlify - cms - app--legacy - peer - deps
+
