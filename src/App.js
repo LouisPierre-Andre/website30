a98a56754fa24e7a6b3a3e19c5b62310013e5ee5
unknown <Louis Pierre Andre>
2024-06-30 08:46:22 -0400

    Description de la modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and welcom to upload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Apprendre React
-        </a>
-      </header>
+         <h1>Welcom to React App</h1>
     </div>
   );
 }
